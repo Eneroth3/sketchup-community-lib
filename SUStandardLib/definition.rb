5d9025366ef567635120cfc1d1f0aa3fca394908
Eneroth3 <eneroth3@gmail.com>
2017-11-29 21:12:56 +0100

    Added ComponentDefinition

---
+++ SUStandardLib/definition.rb
@@ -0,0 +1,55 @@
+module ExampleExtensionModule
+module SUStandardLib
+module ComponentDefinition
+
+  # Get definition used by given instance.
+  # For Versions before SU 2015 there was no Group#definition method.
+  #
+  # @param [Sketchup::ComponentInstance, Sketchup::Group, Sketchup::Image]
+  # @returns [Sketchup::ComponentDefinition]
+  def self.from_instance(instance)
+    if instance.is_a?(Sketchup::ComponentInstance)
+      instance.definition
+    else
+      instance.model.definitions.find { |d| d.instances.include?(instance) }
+    end
+  end
+
+  # Define new axes placement for component.
+  #
+  # @param [Sketchup::ComponentDefiniton]
+  # @param [Geom::Transformation] axes
+  #   A Transformation object defining the new axes placement relative to old
+  #   axes.
+  # @param [Boolean] adjust_instances
+  #   Whether all instances should have their transformations updated to
+  #   compensate for the change in component axes. If +true+ the geometry will
+  #   stay in the same place (in model space), if +false+ the instances' axes
+  #   will stay in the same place.
+  #
+  # @example Move component's axes to its its bottom front left corner
+  #   # Select a component in the model and run:
+  #   definition = Sketchup.active_model.selection.first.definition
+  #   bottom_front_left_pt = definition.bounds.min
+  #   new_axes = Geom::Transformation.new(bottom_front_left_pt)
+  #   SUStandardLib::ComponentDefinition.place_axes(definition, new_axes)
+  #
+  # @return [Void]
+  def self.place_axes(definition, new_axes, adjust_instances = true)
+    definition.entities.transform_entities(
+      new_axes.inverse,
+      definition.entities.to_a
+    )
+
+    if adjust_instances
+      definition.instances.each do |instance|
+        instance.transformation *= new_axes
+      end
+    end
+
+    nil
+  end
+
+end
+end
+end
