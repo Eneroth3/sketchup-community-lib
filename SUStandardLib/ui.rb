eb072cebde2133245f16664a3ab7a19a16778efd
Eneroth3 <eneroth3@gmail.com>
2017-12-04 12:23:37 +0100

    Merge show file and open dir method
    Easier for lib users to just have one method to worry about.

--- SUStandardLib/ui.rb
+++ SUStandardLib/ui.rb
@@ -3,50 +3,37 @@ module SUStandardLib
 # Namespace for methods related to SketchUp's native UI module.
 module UI
 
-  # Open folder.
+  # Open directory and select file if defined.
   #
-  # @param path [String] Path to directory to show.
+  # Commonly referred to in UIs as "Reveal in Finder/Explorer",
+  # "Show Containing Folder" and "Show in Folder".
   #
-  # @example
-  #   # Show directory containing source code for method.
-  #   # In a real life situation when you have a path to a specific file it is
-  #   # likely better to use the show_in_folder method to also select the file
-  #   # than removing the file name from the path and using the open_folder
-  #   # method.
-  #   path = File.dirname(SUStandardLib::UI.method(:show_in_folder).source_location.first)
-  #   SUStandardLib::UI.open_folder(path)
-  #
-  # @return [Void]
-  def self.open_folder(path)
-    if Sketchup.platform == :platform_win
-      system("explorer.exe \"#{path.tr("/", "\\").encode("ISO-8859-1")}\"")
-    else
-      system("open \"#{path}\"")
-    end
-
-    nil
-  end
-
-  # Open directory and select file.
-  #
-  # Commonly referred to as "Reveal in Finder/Explorer" and
-  # "Show Containing Folder".
-  #
-  # @param path [String] Path to file to show.
+  # @param path [String] Path to directory or file to reveal.
   #
   # @example
-  #   # Show file containing source code for method.
-  #   path = SUStandardLib::UI.method(:show_in_folder).source_location.first
-  #   SUStandardLib::UI.show_in_folder(path)
+  #   # Reveal file containing source code for this method.
+  #   path = SUStandardLib::UI.method(:reveal).source_location.first
+  #   SUStandardLib::UI.reveal(path)
+  #
+  #   # Open directory containing source code for this method (without selecting
+  #   # the file).
+  #   path = SUStandardLib::UI.method(:reveal).source_location.first
+  #   dir_path = File.dirname(path)
+  #   SUStandardLib::UI.reveal(dir_path)
   #
   #
   # @return [Void]
-  def self.show_in_folder(path)
-    if Sketchup.platform == :platform_win
-      system("explorer.exe /select,\"#{path.tr("/", "\\").encode("ISO-8859-1")}\"")
-    else
-      system("open -R \"#{path}\"")
-    end
+  def self.reveal(path)
+    raise(ArgumentError, "No such file.") unless File.exist?(path)
+
+    win = Sketchup.platform == :platform_win
+    dir = File.directory?(path)
+    path = path.tr("/", "\\").encode("ISO-8859-1") if win
+
+    command = win ? "explorer.exe " : "open "
+    command << (win ? "/select," : "-R ") unless dir
+    command << "\"#{path}\""
+    system(command)
 
     nil
   end
