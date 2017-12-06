b986adbf9f0f621ab8ccf6cdb6215f675ab1bbee
Eneroth3 <eneroth3@gmail.com>
2017-12-06 21:21:53 +0100

    Renamed method

--- src/su_community_lib/ui.rb
+++ src/su_community_lib/ui.rb
@@ -5,25 +5,25 @@ module UI
 
   # Open directory and select file if defined.
   #
-  # Commonly referred to in UIs as "Reveal in Finder/Explorer",
-  # "Show Containing Folder" and "Show in Folder".
+  # Commonly referred to in UIs as "Open File Location",
+  # "Reveal in Finder/Explorer", "Show Containing Folder" and "Show in Folder".
   #
   # @param path [String] Path to directory or file to reveal.
   #
   # @example
   #   # Reveal file containing source code for this method.
-  #   path = SUCommunityLib::UI.method(:reveal).source_location.first
-  #   SUCommunityLib::UI.reveal(path)
+  #   path = SUCommunityLib::UI.method(:reveal_path).source_location.first
+  #   SUCommunityLib::UI.reveal_path(path)
   #
   #   # Open directory containing source code for this method (without selecting
   #   # the file).
-  #   path = SUCommunityLib::UI.method(:reveal).source_location.first
+  #   path = SUCommunityLib::UI.method(:reveal_path).source_location.first
   #   dir_path = File.dirname(path)
-  #   SUCommunityLib::UI.reveal(dir_path)
+  #   SUCommunityLib::UI.reveal_path(dir_path)
   #
   #
   # @return [Void]
-  def self.reveal(path)
+  def self.reveal_path(path)
     raise(ArgumentError, "No such file.") unless File.exist?(path)
 
     win = Sketchup.platform == :platform_win
