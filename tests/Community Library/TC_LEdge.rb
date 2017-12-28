243bf4c6505ff1784e4d260106704acef816e098
Eneroth3 <eneroth3@gmail.com>
2017-12-28 12:21:55 +0100

    Unit test LEdge

---
+++ tests/Community Library/TC_LEdge.rb
@@ -0,0 +1,27 @@
+require 'testup/testcase'
+
+class TC_LEdge < TestUp::TestCase
+
+  LEdge = SUCommunityLib::LEdge
+
+  def setup
+    #...
+  end
+
+  def teardown
+    #...
+  end
+
+  #-----------------------------------------------------------------------------
+
+  def test_midpoint
+    pts = [Geom::Point3d.new(0, 0, 0), Geom::Point3d.new(2, 2, 2)]
+    edge = Sketchup.active_model.active_entities.add_edges(pts).first
+    midpoint = LEdge.midpoint(edge)
+    expectation = Geom::Point3d.new(1, 1, 1)
+    edge.erase!
+
+    assert_equal(expectation, midpoint)
+  end
+
+end
