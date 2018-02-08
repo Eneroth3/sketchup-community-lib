90aa7c26cff911cf19143c73b9bb48eef8db79dd
Eneroth3 <eneroth3@gmail.com>
2018-02-08 13:39:15 +0100

    Make example use library module
    Previosly defined the make unique action itself.

--- Examples/deep_make_unique.rb
+++ Examples/deep_make_unique.rb
@@ -14,75 +14,8 @@ module Examples
 module DeepMakeUnique
 
   ACTION_NAME = "Deep Make Unique".freeze
-  DEBUG_MODE = false
 
-  # Used for debugging. Draws an edge within a definition so its instances can
-  # easily be visually distinguished.
-  #
-  # @param definition [Sketchup::ComponentDefiniton]
-  #
-  # @return [Void]
-  def self.mark_definition(definition)
-    definition.entities.add_line(ORIGIN, [0, 0, 100])
-  end
-
-  # Recursively traverses instance and child instances and make them unique to
-  # the defined scope.
-  #
-  # @param instance [Sketchup::Group, Sketchup::ComponentInstance]
-  # @param scope [Array<Sketchup::Group, Sketchup::ComponentInstance>]
-  # @replacement_table [Hash]
-  #
-  # @return [Void]
-  def self.recursive_make_unique(instance, scope, replacement_table)
-    definition = SkippyLib::LEntity.definition(instance)
-
-    if replacement_table[definition]
-      SkippyLib::LEntity.swap_definition(instance, replacement_table[definition])
-      return
-    end
-
-    # A new unique definition is only created if the definition has instances
-    # outside of the scope. If the definition is already unique to the scope
-    # this instance doesn't need to be made unique.
-    unless SkippyLib::LComponentDefinition.unique_to?(definition, scope)
-      instance.make_unique
-      old_definition = definition
-      definition = SkippyLib::LEntity.definition(instance)
-      replacement_table[old_definition] = definition
-    end
-
-    definition.entities.each do |i|
-      next unless SkippyLib::LEntity.instance?(i)
-      recursive_make_unique(i, scope, replacement_table)
-    end
-
-    mark_definition(definition) if DEBUG_MODE
-
-    nil
-  end
-
-  # Make all definitions within scope unique to that scope.
-  #
-  # @param scope [Array<Sketchup::Group, Sketchup::ComponentInstance>]
-  #
-  # @returns [Nothing].
-  def self.deep_make_unique(scope)
-    # Instance within the scope that already use a shared definition should keep
-    # using a shared definition. Once one instance is made unique its new
-    # definition is remembered and re-used for the others.
-    replacement_table = {}
-
-    scope.each { |i| recursive_make_unique(i, scope, replacement_table) }
-
-    nil
-  end
-
-  # Wrapper for deep_make_unique. Starts model operation and make the current
-  # selection deep unique.
-  #
-  # @returns [Nothing]
-  def self.perform_deep_make_unique_operation
+  def self.deep_make_unique
     model = Sketchup.active_model
 
     scope = model.selection.select { |i| SkippyLib::LEntity.instance?(i) }
@@ -91,7 +24,7 @@ module DeepMakeUnique
     old_definition_count = model.definitions.size
 
     model.start_operation(ACTION_NAME, true)
-    deep_make_unique(scope)
+    SkippyLib::DeepMakeUnique.deep_make_unique(scope)
     model.commit_operation
 
     new_definitions_count = model.definitions.size - old_definition_count
@@ -149,7 +82,7 @@ module DeepMakeUnique
     UI.add_context_menu_handler do |menu|
       next unless menu_available?
       SkippyLib::LUI.add_menu_item(menu, ACTION_NAME, menu_index) do
-        perform_deep_make_unique_operation
+        deep_make_unique
       end
     end
   end
